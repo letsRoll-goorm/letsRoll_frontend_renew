68fdccb2f7ce41db585c4c3d6c87899005aa033f
yusuShin <tlsdbtn0507@naver.com>
2024-01-08 19:32:42 +0900

    first commit

---
+++ src/pages/main/Main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import * as S from "./style";
+
+function Main() {
+  return (
+    <>
+      <S.MainTitle>메인페이지</S.MainTitle>
+      <div style={{ padding: "40px" }}>
+      </div>
+    </>
+  );
+}
+
+export default Main;
